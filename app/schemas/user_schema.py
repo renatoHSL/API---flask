d89bb81d3f7e81fe27e02d21769fcc66af827ca6
Renato <renato92.hora@gmail.com>
2024-12-18 10:55:56 -0300

    update: adicionado mais algumas validacoes em user schema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,12 +1,30 @@
 from marshmallow import Schema, fields, validate, pre_load
+from marshmallow import ValidationError
+
+
+def validate_username(value):
+    if not value.isalnum():
+        raise ValidationError("Username must contain only letters and numbers.")
 
 
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
-    username = fields.Str(validate=[validate.Length(min=3, max=20)], required=True)
-    email = fields.Str(required=True, validate=validate.Email(error="Not a valid email address"))
+    username = fields.Str(
+        validate=[
+            validate.Length(min=3, max=20),
+            validate_username
+        ],
+        required=True
+    )
+    email = fields.Str(
+        required=True,
+        validate=validate.Email(error="Invalid email format. Please enter a valid email.")
+    )
     password = fields.Str(
-        attribute="password_hash", required=True, validate=[validate.Length(min=6, max=36)], load_only=True
+        attribute="password_hash",
+        required=True,
+        validate=[validate.Length(min=6, max=36)],
+        load_only=True
     )
 
     @pre_load
